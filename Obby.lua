d6d843ce6c00f81c33f59a436213d4eafd372193
Alalakskslk⁤ <135785245+Azlz6@users.noreply.github.com>
2023-10-24 14:48:31 -0300

    Update Obby.lua

--- Obby.lua
+++ Obby.lua
@@ -1,3 +1,1 @@
-while true do
-  wait(0.0001)
 for _,v in pairs(game:GetDescendants()) do if v.ClassName == "RemoteEvent" then if v.Parent.Name == "WeaponsRemotes" or v.Parent.Name == "VipRemotes" or v.Parent.Name == "Remotes" then v:FireServer() end end end
